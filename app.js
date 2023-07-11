ceb8764296e3797f22e96238f0ca03f02875b378
Marcello Alfaro <marcello.alfaro1@gmail.com>
2023-07-11 18:36:41 -0500

    Added upload-download folder feature and minor fixes

--- app.js
+++ app.js
@@ -19,22 +19,24 @@ const socket = io(`${API_URL}${SOCKET_NAMESPACE}`, {
 
 socket.on('connect', () => console.log('Connection with main server established!'));
 
-socket.on('alloc-storage-server', async ({ filePartId, dirId, filename }) => {
+socket.on('alloc-storage-server', async ({ filePartId, transferId, filename }) => {
   try {
+    const path = `storage/${transferId}`;
+
     try {
-      await fsp.access(`storage/${dirId}`);
+      await fsp.access(path);
     } catch (err) {
-      await fsp.mkdir(`storage/${dirId}`, { recursive: true });
+      await fsp.mkdir(path, { recursive: true });
     }
 
-    const req = await fetch(`${API_URL}/files/transfer/storage-server`, {
+    const response = await fetch(`${API_URL}/files/transfer/storage-server`, {
       headers: {
         Authorization: `Bearer ${token}`,
         filePartId,
       },
     });
 
-    await pipeline(req.body, fs.createWriteStream(`storage/${dirId}/${filename}`, { flags: 'a' }));
+    await pipeline(response.body, fs.createWriteStream(`${path}/${filename}`, { flags: 'a' }));
 
     socket.emit(filePartId, `ACK ${filePartId}`);
   } catch (err) {
@@ -45,17 +47,57 @@ socket.on('alloc-storage-server', async ({ filePartId, dirId, filename }) => {
 
 socket.on('get-file', async (data) => {
   try {
-    const { requestId, single, dirId } = data;
+    const { requestId, single, isfolder, folder, transferId, transfer, fileId } = data;
+
+    if (isfolder) {
+      const zip = new JSZip();
+      const root = zip.folder(folder.name);
+
+      folder.Files.forEach(({ fileId, name, path }) => {
+        root.folder(path).file(name, fs.createReadStream(`storage/${transferId}/${fileId}`));
+      });
+
+      const body = zip.generateNodeStream({ streamFiles: true });
+
+      return await fetch(`${API_URL}/files/get-file`, {
+        method: 'PUT',
+        body,
+        headers: {
+          requestId,
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    }
 
     const body = single
-      ? fs.createReadStream(`storage/${dirId}/${data.fileId}`)
+      ? fs.createReadStream(`storage/${transferId}/${fileId}`)
       : (() => {
-          const zip = new JSZip();
-          const folder = zip.folder(data.title);
+          const { Files = [], Folders = [] } = transfer;
+          const files = [...Files, ...Folders];
 
-          data.Files.forEach((entry) =>
-            folder.file(entry.name, fs.createReadStream(`storage/${dirId}/${entry.fileId}`))
-          );
+          const zip = new JSZip();
+          const root = zip.folder(transfer.title);
+
+          files.forEach((file) => {
+            if (file?.fileId)
+              return root.file(
+                file.name,
+                fs.createReadStream(`storage/${transferId}/${file.fileId}`)
+              );
+
+            if (file?.folderId) {
+              const rootfolder = root.folder(file.name);
+
+              file.Files.forEach((folderFile) =>
+                rootfolder
+                  .folder(folderFile.path)
+                  .file(
+                    folderFile.name,
+                    fs.createReadStream(`storage/${transferId}/${folderFile.fileId}`)
+                  )
+              );
+            }
+          });
           return zip.generateNodeStream({ streamFiles: true });
         })();
 
@@ -74,7 +116,7 @@ socket.on('get-file', async (data) => {
 
 socket.on(
   'unlink-file',
-  async ({ dirId }) => await fsp.rm(`storage/${dirId}`, { recursive: true, force: true })
+  async ({ transferId }) => await fsp.rm(`storage/${transferId}`, { recursive: true, force: true })
 );
 
 socket.on('connect_error', (err) => {
