40cd7f4edb3123e84eb5795cf7586e9666545067
Marcello Alfaro <marcello.alfaro1@gmail.com>
2024-08-20 17:02:35 -0500

    Minor tweaks and fixes and added a new ws lib to reconnect when server's connection fails

--- app.js
+++ app.js
@@ -1,222 +1,213 @@
-import { API_URL, API_PATH, JWT_SECRET, SOCKET_CHANNEL } from './config/config.js';
+import { API_URL, API_PATH, JWT_SECRET, SOCKET_NAMESPACE } from './config/config.js';
 import server from './helpers/server.js';
 import logger from './helpers/logger.js';
-import WebSocket from 'ws';
 import fs from 'fs-extra';
+import WebSocket from './helpers/ws.js';
 import { fetch } from 'undici';
-import { randomUUID } from 'crypto';
 import EventEmitter from 'events';
 import { pipeline } from 'stream/promises';
 import archiver from 'archiver';
 import jwt from 'jsonwebtoken';
+import ErrorObject from './helpers/errorObject.js';
 
 try {
   logger.info(`Server started - Running Node.js version ${process.version}`);
 
   const token = jwt.sign({ id: server.id, name: server.name }, JWT_SECRET);
 
-  (function init() {
-    const socket = new WebSocket(`${API_URL + API_PATH}.uws${SOCKET_CHANNEL}`, {
+  const eventEmitter = new EventEmitter();
+
+  const socket = new WebSocket(
+    `${/* API_URL */ 'http://localhost:8081' + API_PATH}.uws${SOCKET_NAMESPACE}`,
+    {
       headers: {
         authorization: `Bearer ${token}`,
       },
-    });
-    const eventEmitter = new EventEmitter();
+    }
+  );
 
-    socket.sendWithAck = async (message) => {
-      const messageId = randomUUID();
-      socket.send(JSON.stringify({ messageId, ...message }));
+  socket.ack = (messageId) =>
+    new Promise((res) => eventEmitter.on(messageId, (response) => res(response)));
 
-      await new Promise((res) => eventEmitter.once(messageId, (response) => res(response)));
-    };
+  socket.onopen = async function () {
+    try {
+      const { ok } = await this.ack(server.id);
 
-    socket.ack = async (messageId) => {
-      return await new Promise((res) => eventEmitter.on(messageId, (response) => res(response)));
-    };
+      if (!ok)
+        throw new ErrorObject(
+          'Something went wrong while establishing the connection with the server.'
+        );
 
-    socket.on('open', async () => {
+      logger.info('Connection with main server established!');
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  socket.onmessage = async ({ data: message }) => {
+    const data = JSON.parse(message);
+    const { action, messageId } = data;
+
+    if (action === 'fetch-server-info') {
       try {
-        await socket.ack(server.id);
-
-        logger.info('Connection with main server established!');
+        socket.send(
+          JSON.stringify({
+            action: 'server-response',
+            messageId,
+            response: { ok: true, status: 200, server: await server.getInfo() },
+          })
+        );
       } catch (err) {
-        throw err;
+        logger.error(err);
       }
-    });
+    }
 
-    socket.on('message', async (message) => {
-      const data = JSON.parse(message);
-      const { action, messageId } = data;
-
-      if (action === 'fetch-server-info') {
-        try {
-          socket.send(
-            JSON.stringify({
-              action: 'server-response',
-              messageId,
-              response: { ok: true, status: 200, server: await server.getInfo() },
-            })
-          );
-        } catch (err) {
-          logger.error(err);
-        }
+    if (action === 'allocate-transfer') {
+      try {
+        const { diskPath, transferId } = data;
+        await fs.mkdirp(`${diskPath}/storage/${transferId}`);
+        socket.send(
+          JSON.stringify({
+            action: 'server-response',
+            messageId,
+            response: { ok: true, status: 200 },
+          })
+        );
+      } catch (err) {
+        socket.send(
+          JSON.stringify({
+            action: 'server-response',
+            messageId,
+            response: { ok: false, status: 500 },
+          })
+        );
+        logger.error(err);
       }
+    }
 
-      if (action === 'allocate-transfer') {
-        try {
-          const { diskPath, transferId } = data;
-          await fs.mkdirp(`${diskPath}/storage/${transferId}`);
-          socket.send(
-            JSON.stringify({
-              action: 'server-response',
-              messageId,
-              response: { ok: true, status: 200 },
-            })
-          );
-        } catch (err) {
-          socket.send(
-            JSON.stringify({
-              action: 'server-response',
-              messageId,
-              response: { ok: false, status: 500 },
-            })
-          );
-          logger.error(err);
-        }
+    if (action === 'handle-file') {
+      try {
+        const { transferId, fileId, diskPath } = data;
+
+        const { body } = await fetch(`${API_URL + API_PATH}/redirect/storage-server`, {
+          headers: {
+            fileId,
+            transferId,
+            authorization: `Bearer ${token}`,
+          },
+        });
+
+        await pipeline(body, fs.createWriteStream(`${diskPath}/storage/${transferId}/${fileId}`));
+
+        socket.send(
+          JSON.stringify({
+            action: 'server-response',
+            messageId,
+            response: { ok: true, status: 200 },
+          })
+        );
+      } catch (err) {
+        await fs.remove(`${data.diskPath}/storage/${data.transferId}`);
+        logger.error(err);
       }
+    }
 
-      if (action === 'handle-file') {
-        try {
-          const { transferId, fileId, diskPath } = data;
+    if (action === 'fetch-transfer') {
+      try {
+        const { type, downloadId, transfer } = data;
 
-          const { body } = await fetch(`${API_URL + API_PATH}/redirect/storage-server`, {
-            headers: {
-              fileId,
-              transferId,
-              authorization: `Bearer ${token}`,
-            },
-          });
-
-          await pipeline(body, fs.createWriteStream(`${diskPath}/storage/${transferId}/${fileId}`));
-
-          socket.send(
-            JSON.stringify({
-              action: 'server-response',
-              messageId,
-              response: { ok: true, status: 200 },
-            })
-          );
-        } catch (err) {
-          await fs.remove(`${data.diskPath}/storage/${data.transferId}`);
-          logger.error(err);
-        }
-      }
-
-      if (action === 'fetch-transfer') {
-        try {
-          const { type, downloadId, transfer } = data;
-
-          const body = (() => {
-            if (type === 'b5ac9c2b')
-              return fs.createReadStream(
-                `${transfer.Disk.path}/storage/${transfer.transferId}/${transfer.Files[0].fileId}`
-              );
-
-            const zip = archiver('zip', { zlib: { level: 0 } });
-
-            if (type === '08ad027d') {
-              transfer.Folders[0].Files.forEach(({ fileId, path }) => {
-                zip.append(
-                  fs.createReadStream(
-                    `${transfer.Disk.path}/storage/${transfer.transferId}/${fileId}`
-                  ),
-                  { name: path }
-                );
-              });
-
-              zip.finalize();
-
-              return zip;
-            }
-
-            transfer.Files.forEach((file) =>
-              zip.append(
-                fs.createReadStream(
-                  `${transfer.Disk.path}/storage/${transfer.transferId}/${file.fileId}`
-                ),
-                { name: `${transfer.title}/${file.name}` }
-              )
+        const body = (() => {
+          if (type === 'b5ac9c2b')
+            return fs.createReadStream(
+              `${transfer.Disk.path}/storage/${transfer.transferId}/${transfer.Files[0].fileId}`
             );
 
-            transfer.Folders.forEach((folder) => {
-              folder.Files.forEach((folderFile) =>
-                zip.append(
-                  fs.createReadStream(
-                    `${transfer.Disk.path}/storage/${transfer.transferId}/${folderFile.fileId}`
-                  ),
-                  { name: `${transfer.title}/${folderFile.path}` }
-                )
+          const zip = archiver('zip', { zlib: { level: 0 } });
+
+          if (type === '08ad027d') {
+            transfer.Folders[0].Files.forEach(({ fileId, path }) => {
+              zip.append(
+                fs.createReadStream(
+                  `${transfer.Disk.path}/storage/${transfer.transferId}/${fileId}`
+                ),
+                { name: path }
               );
             });
 
             zip.finalize();
 
             return zip;
-          })();
+          }
 
-          await fetch(`${API_URL + API_PATH}/redirect/main-server`, {
-            method: 'PUT',
-            body,
-            duplex: 'half',
-            headers: {
-              downloadId,
-              authorization: `Bearer ${token}`,
-            },
+          transfer.Files.forEach((file) =>
+            zip.append(
+              fs.createReadStream(
+                `${transfer.Disk.path}/storage/${transfer.transferId}/${file.fileId}`
+              ),
+              { name: `${transfer.title}/${file.name}` }
+            )
+          );
+
+          transfer.Folders.forEach((folder) => {
+            folder.Files.forEach((folderFile) =>
+              zip.append(
+                fs.createReadStream(
+                  `${transfer.Disk.path}/storage/${transfer.transferId}/${folderFile.fileId}`
+                ),
+                { name: `${transfer.title}/${folderFile.path}` }
+              )
+            );
           });
-        } catch (err) {
-          logger.error(err);
-        }
+
+          zip.finalize();
+
+          return zip;
+        })();
+
+        await fetch(`${API_URL + API_PATH}/redirect/main-server`, {
+          method: 'PUT',
+          body,
+          duplex: 'half',
+          headers: {
+            downloadId,
+            authorization: `Bearer ${token}`,
+          },
+        });
+      } catch (err) {
+        logger.error(err);
       }
+    }
 
-      if (action === 'remove-transfer') {
-        try {
-          const { diskPath, transferId } = data;
-          await fs.rm(`${diskPath}/storage/${transferId}`, { recursive: true, force: true });
+    if (action === 'remove-transfer') {
+      try {
+        const { diskPath, transferId } = data;
+        await fs.rm(`${diskPath}/storage/${transferId}`, { recursive: true, force: true });
 
-          socket.send(
-            JSON.stringify({
-              action: 'server-response',
-              messageId,
-              response: { ok: true, status: 200 },
-            })
-          );
-        } catch (err) {
-          logger.error(err);
-          socket.send(
-            JSON.stringify({
-              action: 'server-response',
-              messageId,
-              response: { ok: false, status: 500 },
-            })
-          );
-        }
+        socket.send(
+          JSON.stringify({
+            action: 'server-response',
+            messageId,
+            response: { ok: true, status: 200 },
+          })
+        );
+      } catch (err) {
+        logger.error(err);
+        socket.send(
+          JSON.stringify({
+            action: 'server-response',
+            messageId,
+            response: { ok: false, status: 500 },
+          })
+        );
       }
+    }
 
-      if (action === 'main-server-response') {
-        const { messageId, response } = data;
-        eventEmitter.emit(messageId, response);
-      }
-    });
+    if (action === 'main-server-response') return eventEmitter.emit(messageId, data.response);
+  };
 
-    socket.on('close', (code, reason) => {
-      logger.warn(`Connection with main server lost due to ${code}:${reason}`);
-      setTimeout(init, 2000);
-    });
+  socket.onclose = ({ code }) => logger.warn(`Connection with main server closed due to ${code}`);
 
-    socket.on('error', (err) => {
-      logger.error(`Connection error due to ${err}`);
-    });
-  })();
+  socket.onerror = ({ error: { code } }) => logger.error(`Connection error due to ${code}`);
 
   process.on('uncaughtException', (err) => {
     logger.fatal(err);
--- config/config.js
+++ config/config.js
@@ -1,2 +1,2 @@
-export const { API_URL, API_PATH, LOGS_DESTINATION, JWT_SECRET, SOCKET_CHANNEL } = process.env;
+export const { API_URL, API_PATH, LOGS_DESTINATION, JWT_SECRET, SOCKET_NAMESPACE } = process.env;
 export const DISKS = JSON.parse(process.env.DISKS);
